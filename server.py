f9113058e69d83aefb3335a59a4a02c8ee5baa33
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-21 12:15:13 +0530

    create a server

--- server.py
+++ server.py
@@ -2,4 +2,8 @@ import socket
 import threading
 
 port = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
+# access the host address by name
+host = socket.gethostbyname(socket.gethostname())
+# Create a server using socket and add family and stream
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
