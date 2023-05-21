2e3e0ffa390a3f43337d196cb7cf4103c2c94bca
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-21 12:41:24 +0530

    add receive and handle function in server.py

--- server.py
+++ server.py
@@ -4,6 +4,23 @@ import threading
 port = 5050
 # access the host address by name
 host = socket.gethostbyname(socket.gethostname())
-# Create a server using socket and add family and stream
+addr = (host, port)
+# Create a server using socket and add family and type
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(__address=addr)
 
+def handle(client):
+    while True:
+        try:
+            message = client.
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print()
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is listing....")
+server_thread = threading.Thread(target=receive)
+server_thread.start()
