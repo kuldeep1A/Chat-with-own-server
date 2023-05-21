5b55d5cc2bf713c6a095aef45e912b2c6211fa8b
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-21 13:05:53 +0530

    added some logic code on function

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import socket
 import threading
+from base64 import encode
+from tarfile import ENCODING
 
 port = 5050
+ENCODE = "ascii"
 # access the host address by name
 host = socket.gethostbyname(socket.gethostname())
 addr = (host, port)
@@ -9,17 +12,49 @@ addr = (host, port)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(__address=addr)
 
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
 def handle(client):
     while True:
+
         try:
-            message = client.
+            message = client.recv(1024)
+            broadcast(message)
+
+        except:  # When client disconnects
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            print(f"{nickname} left the chat!".encode(ENCODING))
+            nicknames.remove(nickname)
+            break
+
 
 def receive():
     while True:
         client, address = server.accept()
-        print()
+        print(f"Connect with {str(address)}")
+        client.send("NICK".encode(ENCODING))
+        nickname = client.recv(1024).decode(ENCODING)
+
+        clients.append(client)
+        nicknames.append(nickname)
+
+        print(f"Nickname of the client is {nickname}!")
+        broadcast(f"{nickname} joined the chat.".encode(ENCODING))
+        client.send('Connect to the server!'.encode(ENCODING))
+
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
 
 print("Server is listing....")
 server_thread = threading.Thread(target=receive)
